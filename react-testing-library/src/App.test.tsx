468422297e065b738007c4d173fc1e7f6b8a7876
VadymPertsov <v.pertsov@quantumobile.com>
2024-03-20 12:28:15 +0200

    init async test

--- react-testing-library/src/App.test.tsx
+++ react-testing-library/src/App.test.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('First Test', () => {
-  render(<App />);
-  const titleHelloWorld = screen.getByText(/testing/i);
-  const input = screen.getByPlaceholderText(/input/i);
-  const btn = screen.getByRole('button');
+describe('Testing react app', () => {
+  test('Components in the document', () => {
+    render(<App />);
+    const titleHelloWorld = screen.getByText(/testing/i);
+    const input = screen.getByPlaceholderText(/input/i);
+    const btn = screen.getByRole('button');
+  
+    expect(titleHelloWorld).toBeInTheDocument(); 
+    expect(input).toBeInTheDocument(); 
+    expect(btn).toBeInTheDocument(); 
+  });
 
-  expect(titleHelloWorld).toBeInTheDocument(); 
-  expect(input).toBeInTheDocument(); 
-  expect(btn).toBeInTheDocument(); 
-});
+  test('First Test', async () => {
+    render(<App />);
+    const titleHelloWorld = await screen.findByText(/John/i);
+    expect(titleHelloWorld).toBeInTheDocument();
+  });
+})
--- react-testing-library/src/App.tsx
+++ react-testing-library/src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState<null | { name: string }>(null)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setData({name: 'John'})
+    }, 100)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div>
+      {data && <div>{data.name}</div>}
       <h1>Testing</h1>
       <input type="text" placeholder='INPUT' />
       <button>BUTTON</button>
